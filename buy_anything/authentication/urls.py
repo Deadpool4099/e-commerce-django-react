c2e5ea996c724d3229fc32448f593d951f49abf7
Ayushi Singh <ayushisingh@Ayushis-MacBook-Pro.local>
2024-03-17 23:41:30 +0530

    Updating the files and logic of view to class based views from functional based views

--- buy_anything/authentication/urls.py
+++ buy_anything/authentication/urls.py
@@ -1,11 +1,22 @@
-from django.contrib import admin
-from django.urls import path,include
-from . import views
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import SignupView, ActivateView, SigninView, SignoutView, HomeView
 
+# Define DRF router
+router = DefaultRouter()
+
+# Register your views with the router
+# router.register(r'signup', SignupView, basename='signup')
+# router.register(r'signout', SignoutView, basename='signout')
+
+# Define your custom paths
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('signup', views.signup, name='signup'),
-    path('activate/<uidb64>/<token>', views.activate, name='activate'),
-    path('signin', views.signin, name='signin'),
-    path('signout', views.signout, name='signout'),
+    path('', HomeView.as_view(), name='home'),
+    path('activate/<uidb64>/<token>', ActivateView.as_view(), name='activate'),
+    path('signin', SigninView.as_view(), name='signin'),
+    path('signout', SignoutView.as_view(), name='signout'),
+    path('signup', SignupView.as_view(), name='signup'),
 ]
+
+# Include the DRF router URLs
+urlpatterns += router.urls
--- buy_anything/authentication/views.py
+++ buy_anything/authentication/views.py
@@ -1,73 +1,97 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+# Import DRF modules
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+
+# Import DRF serializers
+from .serializers import UserSerializer
+
+# Import Django authentication
+from django.contrib.auth import authenticate, login, logout
+from django.utils.encoding import force_bytes, force_str
+# Import Django User model
 from django.contrib.auth.models import User
+
+# Import Django messages framework
 from django.contrib import messages
-from django.core.mail import EmailMessage, send_mail
-from buy_anything import settings
+
+# Import other necessary modules
+from .tokens import generate_token
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
-from django.utils.encoding import force_bytes, force_str
-from django.contrib.auth import authenticate, login, logout
+from django.core.mail import EmailMessage
+
+
+from django.shortcuts import render
+
+class HomeView(APIView):
+    def get(self, request):
+        return render(request, 'authentication/index.html')
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.views import View
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.core.mail import send_mail, EmailMessage
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.conf import settings
 from .tokens import generate_token
 
+class SignupView(View):
+    def get(self, request):
+        return render(request, "authentication/signup.html")
 
-# Create your views here.
-def home(request):
-    return render(request, "authentication/index.html")
+    def post(self, request):
+        username = request.POST.get('username')
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
 
-
-
-def signup(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        fname = request.POST['fname']
-        lname = request.POST['lname']
-        email = request.POST['email']
-        pass1 = request.POST['pass1']
-        pass2 = request.POST['pass2']
-
-        if User.objects.filter(username=username):
-            messages.error(request, "Username already exist! Please try some other username.")
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists! Please try another username.")
             return redirect('home')
 
         if User.objects.filter(email=email).exists():
-            messages.error(request, "Email Already Registered!!")
+            messages.error(request, "Email already registered!")
             return redirect('home')
 
         if len(username) > 20:
-            messages.error(request, "Username must be under 20 charcters!!")
+            messages.error(request, "Username must be under 20 characters!")
             return redirect('home')
 
         if pass1 != pass2:
-            messages.error(request, "Passwords didn't matched!!")
+            messages.error(request, "Passwords don't match!")
             return redirect('home')
 
         if not username.isalnum():
-            messages.error(request, "Username must be Alpha-Numeric!!")
+            messages.error(request, "Username must be alphanumeric!")
             return redirect('home')
 
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
-        # myuser.is_active = False
         myuser.is_active = False
         myuser.save()
-        messages.success(request,
-                         "Your Account has been created succesfully!! Please check your email to confirm your email address in order to activate your account.")
+
+        messages.success(request, "Your account has been created successfully! Please check your email to confirm your email address in order to activate your account.")
 
         # Welcome Email
-        subject = "Welcome to BuyAnything.com- Django Login!!"
-        message = "Hello " + myuser.first_name + "!! \n" + "Welcome to BuyAnything.com!! \nThank you for visiting our website\n. We have also sent you a confirmation email, please confirm your email address. \n\nThanking You\nAnubhav Madhav"
+        subject = "Welcome to BuyAnything.com - Django Login!!"
+        message = f"Hello {myuser.first_name}!\nWelcome to BuyAnything.com!\nThank you for visiting our website. We have also sent you a confirmation email, please confirm your email address.\n\nThank you,\n"
         from_email = settings.EMAIL_HOST_USER
-        to_list = [myuser.email]
-        send_mail(subject, message, from_email, to_list, fail_silently=True)
+        to_email = [myuser.email]
+        send_mail(subject, message, from_email, to_email, fail_silently=True)
 
         # Email Address Confirmation Email
         current_site = get_current_site(request)
         email_subject = "Confirm your Email @ BuyAnything.com - Django Login!!"
         message2 = render_to_string('email_confirmation.html', {
-
             'name': myuser.first_name,
             'domain': current_site.domain,
             'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
@@ -84,47 +108,46 @@ def signup(request):
 
         return redirect('signin')
 
-    return render(request, "authentication/signup.html")
 
+class ActivateView(APIView):
+    permission_classes = [AllowAny]
 
-def activate(request, uidb64, token):
-    try:
-        uid = force_str(urlsafe_base64_decode(uidb64))
-        myuser = User.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        myuser = None
+    def get(self, request, uidb64, token):
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
 
-    if myuser is not None and generate_token.check_token(myuser, token):
-        myuser.is_active = True
-        # user.profile.signup_confirmation = True
-        myuser.save()
-        login(request, myuser)
-        messages.success(request, "Your Account has been activated!!")
-        return redirect('signin')
-    else:
-        return render(request, 'activation_failed.html')
+        if user is not None and generate_token.check_token(user, token):
+            user.is_active = True
+            user.save()
+            login(request, user)
+            messages.success(request, "Your Account has been activated!!")
+            return redirect('signin')
+        else:
+            return Response({'error': 'Invalid activation link'}, status=status.HTTP_400_BAD_REQUEST)
 
+class SigninView(View):
+    def get(self, request):
+        return render(request, "authentication/signin.html")
 
-def signin(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        pass1 = request.POST['pass1']
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('pass1')
 
-        user = authenticate(username=username, password=pass1)
+        user = authenticate(username=username, password=password)
 
         if user is not None:
             login(request, user)
             fname = user.first_name
-            # messages.success(request, "Logged In Sucessfully!!")
             return render(request, "authentication/index.html", {"fname": fname})
         else:
-            messages.error(request, "Bad Credentials!!")
+            messages.error(request, "Bad Credentials!")
             return redirect('home')
 
-    return render(request, "authentication/signin.html")
-
-
-def signout(request):
-    logout(request)
-    messages.success(request, "Logged Out Successfully!!")
-    return redirect('home')
+class SignoutView(View):
+    def get(self, request):
+        logout(request)
+        messages.success(request, "Logged Out Successfully!")
+        return redirect('home')
