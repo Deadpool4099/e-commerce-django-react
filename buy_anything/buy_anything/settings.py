13b2f0e50acb65185ac224d961988e3efa4e1763
Ayushi Singh <ayushisingh@Ayushis-MacBook-Pro.local>
2024-03-17 21:54:32 +0530

    Adding files and directories with function based user authentication

--- buy_anything/buy_anything/settings.py
+++ buy_anything/buy_anything/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from . info import *
+from .db_settings import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+EMAIL_USE_TLS = EMAIL_USE_TLS
+EMAIL_HOST = EMAIL_HOST
+EMAIL_HOST_USER = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+EMAIL_PORT = EMAIL_PORT
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'buy_anything.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,22 +80,7 @@ WSGI_APPLICATION = 'buy_anything.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST':'localhost',
-        'PORT':'5432',
-        'USER':'tejaswi.kompella',
-        'PASSWORD':'Tejaswi@Kompella@99',
-        'NAME':'buy_anything'
-
-
-# 'ENGINE': '',
-#         'NAME': 'myDBname',
-#         'USER': 'myUser',
-#         'PASSWORD': 'myPass',
-#         'HOST': '127.0.0.1',
-#         'PORT': '8000',
-    }
+    'default' : db_settings
 }
 
 
