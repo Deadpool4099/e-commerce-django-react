556978b545e8b1f4fadbb35a3122bc6c2034b43e
Tejaswi Kompella <tejaswi.kompella@aidash.com>
2024-03-25 16:33:16 +0530

    basic get method apis in api app

--- buy_anything/api/views.py
+++ buy_anything/api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode
+
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_products(request):
+    pass
+
+
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication, SessionAuthentication])
+@permission_classes([IsAuthenticated])
+def get_cart_items(request):
+    # token_value = request.headers['Authorization']
+    # key = token_value.split(' ')[-1]
+    # user = Token.objects.get(key=key).user
+    pass
