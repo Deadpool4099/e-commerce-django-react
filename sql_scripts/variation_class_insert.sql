edc540259be4f9d251454680a7eaff08ea2d445e
Deadpool4099 <tejaswikompella@Tejaswis-MacBook-Air.local>
2024-11-06 00:29:38 +0530

    Resolved Conflicts

---
+++ sql_scripts/variation_class_insert.sql
@@ -0,0 +1,29 @@
+INSERT INTO public.variation_class (id,value,variation_type_id) VALUES
+	 ('5c376e31-ac12-4e4f-a424-176c2f9e8d05','32E','732d376a-3176-4a2a-bca3-8b64cd781c44'),
+	 ('170f169b-81ae-4e4c-b50e-4013e07c0364','36E','732d376a-3176-4a2a-bca3-8b64cd781c44'),
+	 ('2eb8d882-faf8-4f0f-b65c-1e2052afbd1a','32E','8c1920d8-fef8-4248-b360-2886510225df'),
+	 ('ef42f32d-daac-4af2-ba72-30fa4d97ed8d','White','e22500cb-61b5-4742-983c-141cb53d53b8'),
+	 ('a4307e90-6d6d-40fe-981f-4645ec9d1e38','Pink','e22500cb-61b5-4742-983c-141cb53d53b8'),
+	 ('59288222-19f8-4c1a-89bf-e246572c6ee7','Blue','e22500cb-61b5-4742-983c-141cb53d53b8'),
+	 ('b2d384b4-ff9e-49a4-bebb-d41e3130692e','Cement Grey','e22500cb-61b5-4742-983c-141cb53d53b8'),
+	 ('b605f397-80e7-4ea3-84de-96240db5dc0f','Black','0036b779-716a-4b7b-ae1f-fc1fffd9831b'),
+	 ('7d68ef4e-b8cf-40b1-9928-543da809de00','Blue','0036b779-716a-4b7b-ae1f-fc1fffd9831b'),
+	 ('c7ad8c59-1800-44da-96c8-3fc49bea9665','White','0036b779-716a-4b7b-ae1f-fc1fffd9831b');
+INSERT INTO public.variation_class (id,value,variation_type_id) VALUES
+	 ('17e7a24f-d7d0-44ff-945c-22b8ca725689','Gold','0036b779-716a-4b7b-ae1f-fc1fffd9831b'),
+	 ('e59afa3d-a0bd-471a-82cc-779f903dee72','Black','bf803cc4-bc79-4936-bdcb-10a5cd676d4f'),
+	 ('b57de59f-8606-4ff6-92ab-5485c2393402','Blue','bf803cc4-bc79-4936-bdcb-10a5cd676d4f'),
+	 ('e4aea809-08d2-41db-aafd-915152718382','Pink','bf803cc4-bc79-4936-bdcb-10a5cd676d4f'),
+	 ('b9d9a4cd-5a86-488c-b8c2-65e7dd89df45','Cement Grey','bf803cc4-bc79-4936-bdcb-10a5cd676d4f'),
+	 ('ffef5ec8-9ed7-43ea-9d0f-2621f9646d08','4GB','bd6602cd-5502-44a5-976d-bcfe69df93f5'),
+	 ('e0b7b859-dff0-4884-8c13-07abd2f0fe61','6GB','bd6602cd-5502-44a5-976d-bcfe69df93f5'),
+	 ('74f632e0-3189-4fc4-8ad3-5253a39fdcfc','8GB','bd6602cd-5502-44a5-976d-bcfe69df93f5'),
+	 ('c291ab43-62bb-4f95-ad7f-8c023c050b46','12GB','bd6602cd-5502-44a5-976d-bcfe69df93f5'),
+	 ('87b2b9b0-d1d0-4e5f-81f1-f15fe5ae5fb2','16GB','bd6602cd-5502-44a5-976d-bcfe69df93f5');
+INSERT INTO public.variation_class (id,value,variation_type_id) VALUES
+	 ('1eb837f3-2f94-4f49-962f-2455554dde78','32GB','bd6602cd-5502-44a5-976d-bcfe69df93f5'),
+	 ('367faeab-da4e-4a07-ac9f-8670fcf424ac','2GB','b5d7a939-c3de-434e-ac7b-a3d10374298b'),
+	 ('022c8605-3665-40dd-a60c-8b5ffe74f7b4','4GB','b5d7a939-c3de-434e-ac7b-a3d10374298b'),
+	 ('d24d1e42-39b6-469e-babf-52efeb714b33','6GB','b5d7a939-c3de-434e-ac7b-a3d10374298b'),
+	 ('efcc8659-c231-4b7d-b4b7-f6318634fd3d','8GB','b5d7a939-c3de-434e-ac7b-a3d10374298b'),
+	 ('ac321ef5-c0ce-4181-83ee-14a605509621','12GB','b5d7a939-c3de-434e-ac7b-a3d10374298b');
