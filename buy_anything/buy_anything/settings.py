db0787f0dc987079fbf3632eae66ecfe9e9c57d7
Deadpool4099 <tejaswikompella@Tejaswis-MacBook-Air.local>
2024-11-06 00:36:27 +0530

    Enables CORS in middleware and made db settings configurable

--- buy_anything/buy_anything/settings.py
+++ buy_anything/buy_anything/settings.py
@@ -47,16 +47,18 @@ INSTALLED_APPS = [
     'authentication.apps.AuthenticationConfig',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'buy_anything.urls'
