d045e06ae6fb78b16cd6534f7216afe2ec49dd69
Deadpool4099 <tejaswikompella@Tejaswis-MacBook-Air.local>
2024-11-06 19:29:44 +0530

    Added a mail for email confirmation as well

--- buy_anything/authentication/views.py
+++ buy_anything/authentication/views.py
@@ -7,13 +7,15 @@ from rest_framework import status
 from django.contrib.auth import authenticate, login, logout
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode
 
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
 from .serializers import RegisterSerializer, UserSerializer, SignInSerializer
-from .helpers import send_activation_mail
+from .helpers import send_mail_to_user
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -25,7 +27,16 @@ def signup(request):
         user.set_password(request.data['password'])
         user.save()
         token = Token.objects.create(user=user)
-        send_activation_mail(request, user, token)
+        current_site = get_current_site(request)
+        mail_template_context = {
+            'name': user.first_name,
+            'domain': current_site.domain,
+            'token': token,
+            'uid': urlsafe_base64_encode(force_bytes(user.pk))
+        }
+        mail_subject = "Confirm your Email @ BuyAnything.com"
+        mail_template_name = 'activation_mail.html'
+        send_mail_to_user(user, mail_subject, mail_template_name, mail_template_context)
 
         return Response({'user': serializer.data}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -41,8 +52,6 @@ def signin(request):
     if not user:
         return Response({"error": "Invalid Credentials!"}, status=status.HTTP_404_NOT_FOUND)
     token, created = Token.objects.get_or_create(user=user)
-    # token = Token.objects.create(user=user)
-    # token.save()
     if not token:
         token = created
         
@@ -89,6 +98,9 @@ def activate(request, uidb64, token):
     if user is not None:
         user.is_active = True
         user.save()
+        mail_subject = "Account activated @ BuyAnything.com"
+        mail_template_name = 'email_confirmation.html'
+        send_mail_to_user(user, mail_subject, mail_template_name, {})
         return Response({'success': 'Account Activated'}, status=status.HTTP_200_OK)
     else:
         return Response({'error': 'Invalid activation link'}, status=status.HTTP_400_BAD_REQUEST)
