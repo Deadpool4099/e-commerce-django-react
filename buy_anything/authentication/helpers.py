794c25749689df9ab57636d887a80922766ce6e3
Deadpool4099 <tejaswikompella@Tejaswis-MacBook-Air.local>
2024-11-06 19:29:44 +0530

    Added a mail for email confirmation as well

--- buy_anything/authentication/helpers.py
+++ buy_anything/authentication/helpers.py
@@ -6,23 +6,33 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.conf import settings
 
 
-def send_activation_mail(request, user, token):
+def send_mail_to_user(user, mail_subject, mail_template_name, mail_template_context):
 
     # Email Address Confirmation Email
-    current_site = get_current_site(request)
-    email_subject = "Confirm your Email @ BuyAnything.com - Django Login!!"
-    message2 = render_to_string('email_confirmation.html', {
-        'name': user.first_name,
-        'domain': current_site.domain,
-        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-        'token': token
-    })
+    message = render_to_string(mail_template_name, mail_template_context)
     email = EmailMessage(
-        email_subject,
-        message2,
+        mail_subject,
+        message,
         settings.EMAIL_HOST_USER,
         [user.email],
     )
     email.fail_silently = True
     email.send()
+    
+    # current_site = get_current_site(request)
+    # email_subject = "Confirm your Email @ BuyAnything.com"
+    # message2 = render_to_string('activation_mail.html', {
+    #     'name': user.first_name,
+    #     'domain': current_site.domain,
+    #     'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+    #     'token': token
+    # })
+    # email = EmailMessage(
+    #     email_subject,
+    #     message2,
+    #     settings.EMAIL_HOST_USER,
+    #     [user.email],
+    # )
+    # email.fail_silently = True
+    # email.send()
     
