f53463fd94fa8dca8e9c8c1d22a181fa61a1c209
Deadpool4099 <tejaswikompella@Tejaswis-MacBook-Air.local>
2024-11-10 14:17:28 +0530

    added categiry and sub category serializers

--- buy_anything/api/serializers.py
+++ buy_anything/api/serializers.py
@@ -1,9 +1,6 @@
 from rest_framework import serializers
 from .models import Product, Category, SubCategory
 
-class VariationCombinationSerializer(serializers.ModelSerializer):
-    variation_type = serializers.CharField(source='variation_class.variation_type.name')
-    variation_class = serializers.CharField(source='variation_class.value')
 
 class ProductSerializer(serializers.ModelSerializer):
     
@@ -11,28 +8,17 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ['id', 'name', 'description', 'sub_category']
 
-class ProductItemWithDetailsSerializer(serializers.ModelSerializer):
-    product_name = serializers.CharField(source='product.name')
-    # category = serializers.CharField(source='product.product_category.sub_category.category.name')
-    # sub_category = serializers.SerializerMethodField()
-    """"""
-    product_id = serializers.UUIDField(source='product.id')
-    product_description = serializers.CharField(source='product.description')
-    product_item_price = serializers.IntegerField(source='price')
-    product_item_quantity = serializers.IntegerField(source='stock_available')
-    product_item_discount = serializers.IntegerField(source='discount')
-    # product_sub_category = serializers.CharField(source='product.sub_category.name')
-    # product_category = serializers.CharField(source='product.sub_category.category.name')
 
-    # variation_combination = VariationCombinationSerializer(many=True, source='variationcombination_set')
-
-    # def get_sub_category(self, obj):
-    #     return [obj.product.product_category.sub_category.name]
-
+class CategorySerializer(serializers.ModelSerializer):
+    
     class Meta:
-        model = ProductItem
-        fields = ['id', 'product_id', 'product_name',
-                  'product_description', 'product_item_price', 'product_item_quantity', 'product_item_discount',
-                  'product_sub_category', 'product_category',
-                  'variation_combination']
+        model = Category
+        fields = ['id', 'name', 'description']
+        
+        
+class SubCategorySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = SubCategory
+        fields = ['id', 'name', 'description', 'category']
         
